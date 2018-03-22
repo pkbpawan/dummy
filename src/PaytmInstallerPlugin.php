412509e30bdad8988d1d6266c0633a399115495d
pawanbhatiwal <pkb.pawan@gmail.com>
2018-03-22 15:37:08 +0530

    init paytm project

---
+++ src/PaytmInstallerPlugin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace pkbpawan\Composer\Paytm;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+/**
+ * Description of PaytmInstallerPlugin
+ *
+ * @author Pawan Kumar
+ */
+class PaytmInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
+}
